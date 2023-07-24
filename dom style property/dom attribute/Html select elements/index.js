34161cd9095ecfa2f3ac40f7bfe73ac1c70f5983
Habib16051 <habibmhr143@gmail.com>
2023-07-24 15:44:43 +0600

    dom attribute

---
+++ dom style property/dom attribute/Html select elements/index.js
@@ -0,0 +1,26 @@
+// let div = document.querySelector('div')
+
+// div.id = 'menu'
+
+// Set Attribute
+
+// let btn = document.querySelector("#btn")
+// if(btn){
+//     btn.setAttribute('class', 'Habib')
+// }
+
+// Set Attribute
+// let btn = document.querySelector("#btn")
+// if(btn){
+//     console.log(btn.getAttribute("target"));
+// }
+
+// Remove Attribute
+// let btn = document.querySelector("#btn")
+// if(btn){
+//     console.log(btn.removeAttribute("target"));
+// }
+let btn = document.querySelector("#btn")
+if(btn){
+   console.log( btn.hasAttribute("disabled"))
+}
