07efd1be54d43e29e5eb53ac7f8811455ee9a6d7
habib <habibmhr143@gmail.com>
2023-07-25 09:57:58 +0600

    Events

---
+++ Events/index.js
@@ -0,0 +1,11 @@
+// let btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", (e) => {
+//   alert("Clicked Successfully!");
+// });
+
+//  we can do it another way by calling the function on the onclick option in real time
+
+function clicked(e) {
+  alert("Clicked Successfully!");
+}
